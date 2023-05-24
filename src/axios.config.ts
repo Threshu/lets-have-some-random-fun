e7db97104e4c8930acb3dce2cb994e5d25047fad
sbanach <s.banach@raion.eu>
2023-05-24 16:59:03 +0200

    handle axios errors globally

--- src/axios.config.ts
+++ src/axios.config.ts
@@ -1,24 +1,46 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 import Vue from "vue";
-import Snotify, { SnotifyPosition } from 'vue-snotify';
+import Snotify, { SnotifyPosition } from "vue-snotify";
+
+const ABORTED = "ECONNABORTED";
 
 Vue.use(Snotify, {
 	toast: {
-		position: SnotifyPosition.rightBottom
-	}
+		position: SnotifyPosition.rightBottom,
+	},
 });
 
-const axiosInstance=axios.create({
-	baseURL: 'https://pokeapi.co/api/v2'
+const axiosInstance = axios.create({
+	baseURL: "https://pokeapi.co/api/v2",
 });
 
 axiosInstance.interceptors.response.use(
-	(response) => {
+	(response: AxiosResponse) => {
 		return response;
 	},
-	(error) => {
+	(error: AxiosError) => {
 		Vue.prototype.$snotify.error("Request to database failed");
-		return Promise.reject(error);
+		const statusCode = error?.response?.status;
+		const response = {
+			response: error,
+			errorApi: true,
+			code: statusCode,
+			status: statusCode,
+			errorMessage: error.toString(),
+		};
+		if (response?.response?.code === ABORTED) {
+			const timeout =
+				response?.response?.config?.timeout ?? axios.defaults.timeout;
+			const timeoutMessage = `Error: timeout exceeded ${
+				timeout != null ? `${timeout / 1000}s` : ""
+			}`;
+			Vue.prototype.$snotify.error(timeoutMessage);
+			return {
+				...response,
+				errorMessage: timeoutMessage,
+			};
+		}
+		return response;
 	}
 );
 
